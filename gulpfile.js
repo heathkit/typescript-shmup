d6d4ca0f252967bc9e28501814d5fc98853faddb
Michael Giambalvo <heathkit@gmail.com>
2016-03-29 19:54:34 -0700

    Add clang formatting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-var gulp = require('gulp'),
-  tasklisting = require('gulp-task-listing');
+var gulp = require('gulp');
+var tasklisting = require('gulp-task-listing');
+var clangFormat = require('clang-format');
+var gulpFormat = require('gulp-clang-format');
 
+gulp.task('clang', function() {
+  return gulp.src(['src/**/*.ts'])
+      .pipe(gulpFormat.checkFormat('file', clangFormat))
+      .on('warning', function(e) { console.log(e); });
+});
 
 gulp.task('help', tasklisting);
 gulp.task('default', ['help']);
